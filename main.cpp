6b8aaf2812812dd35a33b71612a5315160f1f426
SeungHwan <seunghwanchoi@outlook.com>
2019-08-29 04:26:44 -0600

    minimum spanning graph! Prim's algorithm!

--- main.cpp
+++ main.cpp
@@ -38,6 +38,55 @@ public:
         }
     }
 
+    vector<pair<char,char>> findMinDistanceToConnectAll(vector<Vertex>& v)
+    {
+        set<char> visited;
+
+        //start from A, find shortest edge from visited list to unvisited
+        visited.insert('A');
+
+        vector<pair<char,char>> answer;
+        int total = 0;
+        while(true)
+        {
+            //find shortest edge from visited list.
+            // int min = 9999;
+            char minChar = 0;
+            char origin = 0;
+            int min = 9999;
+            for(char c : visited)
+            {
+                for(auto m : v[c].connect)
+                {
+                    auto search = visited.find(m.first); //if visited skip
+                    if(search != visited.end())
+                        continue;
+                    
+                    if(m.second <= min)
+                    {
+                        min = m.second;
+                        origin = c;
+                        minChar = m.first;
+                    }
+                    
+                }
+            }
+
+            if(minChar == 0) //can't find any point. done
+                break;
+            
+            //add point to connect
+            answer.push_back(make_pair(origin, minChar));
+            visited.insert(minChar);
+            visited.insert(origin);
+            int distance = v[origin].connect.find(minChar)->second;
+            total += distance;
+            
+        }
+        cout << "total: " << total << endl;
+        return answer;
+    }//EOF
+
 };
 
 
@@ -60,9 +109,17 @@ int main()
     graph.addEdge(v['F'], v['E'], 8 );
     graph.addEdge(v['C'], v['B'], 4 );
     graph.addEdge(v['C'], v['E'], 1 );
+    graph.addEdge(v['C'], v['F'], 6 );
 
     // graph.printGraph(v);
 
+    vector<pair<char,char>> answer = graph.findMinDistanceToConnectAll(v);
+
+
+    for(auto n : answer)
+        cout << n.first << "::" << n.second << endl;
+
+    
 
     return 0;
 }
